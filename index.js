433244e7307ee0efb3f6a5e0db1fd56c0b4c5a0a
Forrest Benton <elliottbenton@gmail.com>
2018-04-11 15:54:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string === ""
+  if string.toLowerCase() === string {
+    return "I can't hear you"
+  }
+  
   if string === ""
   if string === ""
   
